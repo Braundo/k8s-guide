fec59645d2f07df9fea3cd45608bc0b381397c68
Braundo <aaronbraundmeier@gmail.com>
2023-10-13 13:56:27 -0500

    more initial changes

--- js/script.js
+++ js/script.js
@@ -6,10 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.add("dark-mode");
   
     // Dark/Light mode toggle
-    darkModeToggle.addEventListener("click", function() {
-      document.body.classList.toggle("dark-mode");
-      document.body.classList.toggle("light-mode");
-    });
+// Dark/Light mode toggle
+darkModeToggle.addEventListener("click", function() {
+    if (document.body.classList.contains("dark-mode")) {
+      document.body.classList.remove("dark-mode");
+      document.body.classList.add("light-mode");
+      darkModeToggle.innerHTML = "&#127769;";  // Moon icon for light mode
+    } else {
+      document.body.classList.remove("light-mode");
+      document.body.classList.add("dark-mode");
+      darkModeToggle.innerHTML = "&#9728;";  // Sun icon for dark mode
+    }
+  });
+  
   
     // Scroll to top
     scrollTopButton.addEventListener("click", function() {
