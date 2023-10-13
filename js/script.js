a1ab04f3020fe460c897ec63dbb3a5418a135ea6
Braundo <aaronbraundmeier@gmail.com>
2023-10-13 13:42:50 -0500

    more initial changes

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const scrollTopButton = document.getElementById("scrollTop");
+  
+    // Default to dark mode
+    document.body.classList.add("dark-mode");
+  
+    // Dark/Light mode toggle
+    darkModeToggle.addEventListener("click", function() {
+      document.body.classList.toggle("dark-mode");
+      document.body.classList.toggle("light-mode");
+    });
+  
+    // Scroll to top
+    scrollTopButton.addEventListener("click", function() {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  
+    // TODO: Add search functionality
+  });
+  
